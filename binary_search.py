6f0eafca93d2d1f1de639f37499e68a9659d4b2a
Nikolai <nik.vagin1995@mail.ru>
2021-01-09 11:53:10 +0300

    binary search algoritm

---
+++ binary_search.py
@@ -0,0 +1,17 @@
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+def search(your_list, iteam):
+    low = 0
+    hight = len(your_list)-1
+    while low <= hight:
+        midle = (low + hight) // 2
+        guess = your_list[midle]
+        if guess == iteam:
+            return guess
+        elif guess > iteam:
+            hight = midle - 1
+        else:
+            low = midle + 1
+    return None
+
+print(search(my_list, 7))
